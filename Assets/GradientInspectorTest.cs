d17fc9e8c53d6dd2b72ad28e8f7412a2265a6690
Matthew Mason <mattg.mason@outlook.com>
2021-12-23 16:55:05 +0000

    Adding finishing touches

---
+++ Assets/GradientInspectorTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientInspectorTest : MonoBehaviour
+{
+    public Gradient Gradient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
